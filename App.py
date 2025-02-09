6ffa032336c8d3d53d7c54b5040ff0187a5a0c14
jcarreir <jav.racec@gmail.com>
2025-02-09 20:19:07 +0100

    Streamlit Chatbot DONE

--- App.py
+++ App.py
@@ -1,12 +1,21 @@
+# Web application framework
+# -----------------------------------------------------------------------
 import streamlit as st
+
+# Environment and system utilities
+# -----------------------------------------------------------------------
 import os
 import dotenv
 import uuid
 
+# LangChain models and schemas
+# -----------------------------------------------------------------------
 from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage
 
-from src.rag import *
+# Custom modules
+# -----------------------------------------------------------------------
+from src.rag import load_doc_to_db, stream_llm_rag_response, stream_llm_response
 
 # Load dot env
 dotenv.load_dotenv()
@@ -25,8 +34,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-
-# --- Initial Setup ---
+# Initial setup
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
 
@@ -34,30 +42,89 @@ if "rag_sources" not in st.session_state:
     st.session_state.rag_sources = []
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "user", "content": "Hello"},
-        {"role": "assistant", "content": "Hi there! How can I assist you today?"}
-]
+    st.session_state.messages = []
 
-# --- Side Bar LLM API Tokens ---
+# API Key
 with st.sidebar:
 
-    default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
-    with st.popover("OpenAI API Key"):
+    default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""
+    
+    with st.popover("OpenAI API Key", icon="🔒"):
         openai_api_key = st.text_input(
-            "Introduce your OpenAI API Key (https://platform.openai.com/)", 
+            "Introduce your OpenAI API Key", 
             value=default_openai_api_key, 
             type="password",
-            key="openai_api_key",
+            key="openai_api_key"
         )
 
-
-# --- Main Content ---
-# Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
-missing_openai = openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key
+# Checking if the user has introduced the OpenAI API Key
+missing_openai = openai_api_key is None or "sk-" not in openai_api_key
 
 if missing_openai:
     st.warning("⬅️ Please introduce an API Key to continue!")
 
 else:
-    pass
+    # Sidebar
+    with st.sidebar:
+        models = []
+        for model in MODELS:
+            if "openai" in model and not missing_openai:
+                models.append(model.split("/")[-1])
+
+        st.selectbox(
+            "Model", 
+            options=models,
+            key="model",
+            help="Currently, only OpenAI models are supported"
+        )
+
+        # Load RAG documents
+        is_vector_db_loaded = False
+        st.button("Load RAG docs", on_click=load_doc_to_db, type="secondary", disabled = is_vector_db_loaded)
+
+        cols0 = st.columns(2)
+        with cols0[0]:
+            is_vector_db_loaded = ("vector_db" in st.session_state and st.session_state.vector_db is not None)
+            st.toggle(
+                "Use RAG", 
+                value=is_vector_db_loaded, 
+                key="use_rag", 
+                disabled=not is_vector_db_loaded
+            )
+
+        with cols0[1]:
+            st.button("Clear Chat", on_click=lambda: st.session_state.messages.clear(), type="primary")
+
+        temp = st.slider("Temperatue", min_value=0.0, max_value=1.0, value=0.3, step=0.1, help="Select model temperature.")
+
+    # Main chat app
+    llm_stream = ChatOpenAI(
+        api_key=openai_api_key,
+        model_name=st.session_state.model.split("/")[-1],
+        temperature=temp,
+        streaming=True,
+    )
+
+    # Message history
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Prompt and chat
+    if prompt := st.chat_input("Type here your message"):
+        
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            messages = [HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"]) for m in st.session_state.messages]
+
+            if st.session_state.use_rag:
+                st.write_stream(stream_llm_rag_response(llm_stream, messages))
+            else:
+                st.write_stream(stream_llm_response(llm_stream, messages))
